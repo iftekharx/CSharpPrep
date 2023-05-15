708e9ad29e4525998a795064ad97198d1a2b7676
iftekharx <iftekharx86@gmail.com>
2023-05-15 20:57:27 +0600

    first commit

---
+++ CSharpPrep/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpPrep { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // String, double, int, bool, char data types in C#
+            // constants
+            const double pi = 3.1415434;
+
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+
+        }
+    }
+}
